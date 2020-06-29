f2d6c04a29fa6776f689b00a2d83b2bf99aaf720
Lucas Oliveira <lcsgoliveirpt@gmail.com>
2020-06-29 14:12:35 +0100

    First Model-Controller-View - Department

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace SalesWebMVC
+namespace SalesWebMVC.Models
 {
     public class Program
     {
